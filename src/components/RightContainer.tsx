8b552f4fa1176faa41bc5aef02c06bb91ee9d178
Lander <lander.labaka@ikasle.aeg.eus>
2025-01-29 13:55:27 +0100

    [MAIN][UPDATE] Display

--- src/components/RightContainer.tsx
+++ src/components/RightContainer.tsx
@@ -5,15 +5,15 @@ interface RightContainerInterface {
 const RightContainer: React.FC<RightContainerInterface> = ({ showVillagers }) => {
 
     return (
-        <div className='flex flex-col h-full w-[50%] border-1 items-center'>
+        <div className='flex flex-col h-full w-[50%] items-center'>
             <button
                 onClick={showVillagers}
-                className="w-[50%] p-2 mt-[5%] border border-gray-300 rounded"
+                className="w-[50%] p-2 mt-[5%] border-gray-300 rounded"
             >
                 SHOW VILLAGER PROFFESION AND WORKERS
             </button>
 
-            <div className="flex flex-col h-[20%] w-full border-1">
+            <div className="flex flex-col h-[20%] w-full">
 
             </div>
         </div>
