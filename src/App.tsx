e3bd80a7bf575725722ac76cedb5f25311295106
llabaka <lander.labaka@ikasle.aeg.eus>
2025-02-08 11:35:21 +0100

    [REDOING][DELETE] Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,14 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import LeftContainer from './components/LeftContainer';
-import { Towers, VillagerProfessionCount } from './types/MegaEpicFortress';
-import { countVillagersByProfession, getTowersAndGuardsByWeaponType } from './helpers/helpers';
-import { megaEpicFortress } from './data/data';
-import RightContainer from './components/RightContainer';
 
 function App() {
-
-  const [showingTower, setShowingTower] = useState<{ towers: Towers[], guardNames: string[] }>({ towers: [], guardNames: [] });
-  const [showingWorkers, setShowingWorkers] = useState<VillagerProfessionCount>({});
-  const [weaponName, setWeaponName] = useState('');
-
-  useEffect(() => {
-    filterTower();
-  }, [weaponName])
-
-  const filterTower = async () => {
-    let filteredTower = showingTower;
-
-    if (weaponName) {
-      filteredTower = getTowersAndGuardsByWeaponType(megaEpicFortress, weaponName)
-      console.log(filteredTower);
-      setShowingTower(filteredTower)
-    } else {
-      setShowingTower({ towers: [], guardNames: [] });
-    }
-  }
-
-  const showVillagers = () => {
-    const villagers = countVillagersByProfession(megaEpicFortress);
-    setShowingWorkers(villagers);
-  };
-
-  useEffect(() => {
-    console.log(showingWorkers);
-
-  }, [showingWorkers])
 
   return (
     <>
       <div className='flex flex-start h-screen w-screen justify-self-start'>
+{/* 
+        <LeftContainer/>
 
-        <LeftContainer
-          weaponName={weaponName}
-          setWeaponName={setWeaponName}
-          showingTower={showingTower}
-          fortress={megaEpicFortress}
-        />
-
-        <RightContainer
-          showVillagers={showVillagers}
-          showingWorkers={showingWorkers}
-        />
+        <RightContainer/> */}
       </div>
     </>
   )
--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,36 +1,13 @@
 import '@testing-library/jest-dom'
-import { megaEpicFortress } from '../data/data';
-import { countVillagersByProfession, getTowersAndGuardsByWeaponType } from '../helpers/helpers';
 
 describe('Recibe la fortaleza, el arma', () => {
   it('devuelve un array de objetos torre con su nombre y devuelve un array de los nombres de los guardianes', () => {
 
-      // Arrange
-      const fortress = megaEpicFortress;
-
-      //Act
-      const findTowers = getTowersAndGuardsByWeaponType(fortress, "Ballista");
-
-      //Assert
-      expect(findTowers.guardNames).toContain("Lira the Watcher");
-      expect(findTowers.guardNames).toContain("Garron the Keen-eyed");
-      expect(findTowers.guardNames).toContain("Pesbe");
-
-      expect(findTowers.towers.map(tower => tower.name)).toContain('Tower of Vigilance')
-      expect(findTowers.towers.map(tower => tower.name)).toContain('Tower of Ice')
   })
 })
 
 describe('Recibe la fortaleza. Devuelve las profesionales y la cantidad de profesionales de los pueblerinos', () => {
   it('Devuelve tipo Record <nombre, cantidad>', () => {
 
-      // Arrange
-      const fortress = megaEpicFortress;
-
-      //Act
-      const record = countVillagersByProfession(fortress);
-
-      //Assert
-      expect(record).toStrictEqual({"Acolyte": 237, "Beggar": 97, "Blacksmith": 31, "Farmer": 467, "Healer": 129, "Merchant": 63})  
   })
 })
